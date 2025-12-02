6178f42016b5bf0bd913e8faec172bcdd6ce252a
Roy Loewenberg <Roy@Roys-MacBook-Pro.local>
2025-12-02 21:58:57 +0200

    Add sqlite3 dependency for LOEW server

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ import { fileURLToPath } from "url";
 import { createDbImpl } from "./dbSqlite.js";
 import { OnboardingEngine } from "./onboardingEngine.js";
 
+import path from "path";
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
